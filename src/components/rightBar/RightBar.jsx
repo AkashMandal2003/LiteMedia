e26cfca3393a2b488ce00cb28c6cb6ab5b1523fa
Aakash <iakashman8653@gmail.com>
2023-01-31 10:37:02 +0530

    LiteMedia

---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,108 @@
+import React from "./rightBar.scss";
+
+const RightBar = () => {
+    return (
+        <div className="rightBar">
+            <div className="container">
+                <div className="item">
+                    <span><b>Suggestion For You</b></span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <span>Akash Mandal</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <span>Leo Messi</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item">
+                    <span><b>Latest Activities</b></span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <p><span>Leo Messi</span> changed their cover photo.
+                            </p>
+                            
+                        </div>
+                       <div><span>10 min ago</span></div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <p><span>Leo Messi</span> shared a story.
+                            </p>
+                        </div>
+                       <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <p><span>Leo Messi</span> commented on a post.
+                            </p>
+                        </div>
+                       <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <p><span>Leo Messi</span> liked a post.
+                            </p>
+                        </div>
+                       <span>1 s ago</span>
+                    </div>
+                </div>
+                <div className="item">
+                    <span><b>Online Friends</b></span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <div className="online" />
+                            <span>Leo Messi</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <div className="online" />
+                            <span>Leo Messi</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <div className="online" />
+                            <span>Leo Messi</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <div className="online" />
+                            <span>Leo Messi</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://phantom-marca.unidadeditorial.es/f07a6e2a95fad43b461b40590225e38d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg" alt="" />
+                            <div className="online" />
+                            <span>Leo Messi</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default RightBar
